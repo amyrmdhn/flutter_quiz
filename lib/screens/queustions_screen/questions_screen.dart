dba209b02ec1ab9c3fb9799428db47f8d1011af2
amyrmdhn <amyramadhan99@gmail.com>
2024-02-03 22:12:54 +0700

    Reword the question screen text to reflect current data

--- lib/screens/queustions_screen/questions_screen.dart
+++ lib/screens/queustions_screen/questions_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz/screens/queustions_screen/components/question_text.dart';
 
-
 import 'package:websafe_svg/websafe_svg.dart';
 
-import 'components/answer_button.dart';
+import '../../models/question.dart';
 import '../../constants.dart';
+
+import 'components/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -15,34 +16,45 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 2;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var currentQuestion = questions[currentQuestionIndex];
+
     return Stack(
       children: [
         WebsafeSvg.asset(
           'assets/images/bg.svg',
           fit: BoxFit.fill,
         ),
-        const SafeArea(
+        SafeArea(
           child: Center(
             child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: defaultPadding),
+              padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
               child: Column(
                 children: [
-                  Spacer(flex: 2),
+                  const Spacer(flex: 2),
                   StyledText(
-                    text: 'Question 1/6',
+                    text:
+                        'Question ${currentQuestionIndex + 1}/${questions.length}',
                     fontSize: 26.0,
                     fontWeight: FontWeight.bold,
                   ),
-                  Spacer(),
+                  const Spacer(),
                   QuestionText(
-                    text: 'How many types of widgets are there in Flutter?',
+                    text: currentQuestion.question,
                     fontSize: 24.0,
                     fontWeight: FontWeight.w600,
                     textAlign: TextAlign.center,
                   ),
-                  Spacer(),
+                  const Spacer(),
                   AnswerButton(),
                   AnswerButton(),
                   AnswerButton(),
